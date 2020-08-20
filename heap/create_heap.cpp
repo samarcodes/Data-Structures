ed578f36d67d9d54e636a4a569d0c3ea13850120
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2020-08-20 21:26:58 +0530

    added heap code

--- heap/create_heap.cpp
+++ heap/create_heap.cpp
@@ -18,7 +18,28 @@ void createHeap(int a[], int n) {
 	}
 }
 
-void Delete(int a[], int n) {
+void heapify(int a[], int n) {
+	for (int i = n / 2; i > 0; i--) {
+		int j = 2 * i ;
+		int k = i;
+		while (j < n) {
+			//compare left and right children of current i
+			if (a[j + 1] > a[j]) {
+				j += 1;
+			}
+			if (a[k] < a[j]) {
+				swap(a[k], a[j]);
+				k = j;
+				j *= 2;
+			}
+			else {
+				break;
+			}
+		}
+	}
+}
+
+void deleteHeap(int a[], int n) {
 	int x = a[1];
 	int i = 1;
 	int j = 2 * i;
@@ -41,25 +62,7 @@ void Delete(int a[], int n) {
 	a[n - 1] = x;
 }
 
-void heapify(int a[], int n) {
-	for (int i = n / 2 - 1; i > 0; i--) {
-		int j = 2 * i + 1;
-		while (j < n - 1) {
-			//compare left and right children of current i
-			if (a[j + 1] > a[j]) {
-				j += 1;
-			}
-			if (a[i] < a[j]) {
-				swap(a[i], a[j]);
-			}
-			else {
-				break;
-			}
-		}
-	}
-}
-
-void show(int a[], int n) {
+void showHeap(int a[], int n) {
 	for (int i = 1; i < n; i++) {
 		cout << a[i] << " ";
 	}
@@ -67,14 +70,14 @@ void show(int a[], int n) {
 }
 
 int main() {
-	int a[] = {0, 40, 35, 30, 15, 10, 25, 5};
+	int a[] = {0, 10, 20, 30, 5, 40, 50};
 	int n = sizeof(a) / sizeof(int);
 
 	heapify(a, n);
 
 	for (int i = 0; i < n - 1; i++)
-		Delete(a, n - i);
+		deleteHeap(a, n - i);
 
-	show(a, n);
+	showHeap(a, n);
 	return 0;
 }
