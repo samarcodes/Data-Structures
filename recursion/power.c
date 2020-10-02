609a13d260a8a9b2b88493fd195a770b93f23f5c
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2020-10-02 20:24:47 +0530

    Update power.c

--- recursion/power.c
+++ recursion/power.c
@@ -1,30 +1,31 @@
 //program to find power using recursion
 //26-01-2020
 #include<stdio.h>
-int power(int,int);
-int Epower(int,int); //efficient power function
+int Power(int, int); //O(n)
+int fastPower(int, int); //efficient power function O(logn)
 int main()
 {
-    int base,exponent;
+    int base, exponent;
     printf("enter base : ");
-    scanf("%d",&base);
+    scanf("%d", &base);
     printf("enter exponent : ");
-    scanf("%d",&exponent);
-    printf("%d raised to %d = %d",base,exponent,Epower(base,exponent));
-}
-int power(int base,int exponent)
-{
-    if(exponent==0)
-        return 1;
-     return(base * power(base,exponent-1));
+    scanf("%d", &exponent);
+    printf("%d raised to %d = %d", base, exponent, FastPower(base, exponent));
 }
 
-int Epower(int base,int exponent)
+int Power(int base, int exponent)
 {
-    if(exponent==0)
+    if (exponent == 0)
         return 1;
-    if(exponent%2==0)
-        return(Epower(base*base,exponent/2));
+    return (base * power(base, exponent - 1));
+}
+
+int fastPower(int base, int exponent)
+{
+    if (exponent == 0)
+        return 1;
+    if (exponent % 2 == 0)
+        return (Epower(base * base, exponent / 2));
     else
-        return(base*Epower(base*base,(exponent-1)/2));
+        return (base * Epower(base * base, (exponent - 1) / 2));
 }
