426cb294cd05bebd0b6d535f855a403988052b65
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2020-12-21 15:16:01 +0530

    Update power.c
    basic formatting

--- recursion/power.c
+++ recursion/power.c
@@ -5,27 +5,27 @@ int Power(int, int); //O(n)
 int fastPower(int, int); //efficient power function O(logn)
 int main()
 {
-    int base, exponent;
-    printf("enter base : ");
-    scanf("%d", &base);
-    printf("enter exponent : ");
-    scanf("%d", &exponent);
-    printf("%d raised to %d = %d", base, exponent, FastPower(base, exponent));
+	int base, exponent;
+	printf("enter base : ");
+	scanf("%d", &base);
+	printf("enter exponent : ");
+	scanf("%d", &exponent);
+	printf("%d raised to %d = %d", base, exponent, fastPower(base, exponent));
 }
 
 int Power(int base, int exponent)
 {
-    if (exponent == 0)
-        return 1;
-    return (base * power(base, exponent - 1));
+	if (exponent == 0)
+		return 1;
+	return (base * Power(base, exponent - 1));
 }
 
 int fastPower(int base, int exponent)
 {
-    if (exponent == 0)
-        return 1;
-    if (exponent % 2 == 0)
-        return (Epower(base * base, exponent / 2));
-    else
-        return (base * Epower(base * base, (exponent - 1) / 2));
+	if (exponent == 0)
+		return 1;
+	if (exponent % 2 == 0)
+		return (fastPower(base * base, exponent / 2));
+	else
+		return (base * fastPower(base * base, (exponent - 1) / 2));
 }
