f965e5b2734dd91d7896f8a5357b655ec9b0d68c
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2020-12-21 15:17:06 +0530

    Update permutation_strings.c
    basic formatting

--- strings/permutation_strings.c
+++ strings/permutation_strings.c
@@ -1,61 +1,59 @@
 #include<stdio.h>
 
 //backtracking (backtracking uses recursion)
-void swap(int *a,int *b)
+void swap(char *a, char *b)
 {
-    int t=*a;
-    *a=*b;
-    *b=t;
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-void perm(char s[],int k)
+void perm(char s[], int k)
 {
-    static int a[10]={0};
-    static char res[10]={0};
+    static int a[10] = {0};
+    static char res[10] = {0};
     int i;
-    if(s[k]=='\0')
+    if (s[k] == '\0')
     {
-        res[k]='\0';
-        printf("%s ",res);
+        res[k] = '\0';
+        printf("%s ", res);
     }
     else
     {
-        for(i=0;s[i]!='\0';i++)
+        for (i = 0; s[i] != '\0'; i++)
         {
-            if(a[i]==0)
+            if (a[i] == 0)
             {
-                res[k]=s[i];
-                a[i]=1;
-                perm(s,k+1);
-                a[i]=0;
+                res[k] = s[i];
+                a[i] = 1;
+                perm(s, k + 1);
+                a[i] = 0;
             }
         }
     }
 }
 
-//it contains error
-void perm1(char s[],int l,int h)
+
+void perm1(char s[], int l, int h)
 {
     int i;
-    if(l==h)
+    if (l == h)
     {
-        printf("%s ",s);
+        printf("%s ", s);
     }
-    else
+
+    for (i = l; i <= h; i++)
     {
-        for(i=l;i<=h;i++)
-        {
-            swap(&s[l],&s[i]);
-            perm1(s,l+1,h);
-            swap(&s[l],&s[i]);
-        }
+        swap(&s[l], &s[i]);
+        perm1(s, l + 1, h);
+        swap(&s[l], &s[i]);
     }
 }
 
 int main()
 {
-    char str[]="ABC";
-    perm(str,0);
-	return 0;
+    char str[] = "ABC";
+    perm(str, 0);
+    return 0;
 }
 
